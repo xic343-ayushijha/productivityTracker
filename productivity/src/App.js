b652f34d6ae7071cf971b488529152130ec10913
Ayushi Jha <ayushi.jha@encora.com>
2024-02-16 17:40:07 +0530

    added initial files

---
+++ productivity/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Commits from './Commits';
+import Pullrequest from './Pullrequest';
+
+function App() {
+  return (
+    <div className="App">
+     <Commits />
+     <Pullrequest />
+    </div>
+  );
+}
+
+export default App;
---
+++ productivity/src/Commits.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+function Commits (){
+
+    const [commit,setCommit]=useState(null);
+
+    useEffect(()=>{
+        fetch('https://api.github.com/repos/xic343-ayushijha/productivityTracker/commits').then(
+            res=>res.json()).then(data=>{
+                // console.log(data);
+                return setCommit(data[0]);
+            });
+        
+    },[]);
+    return(
+        <>
+        <p>author name : {commit?.author?.login}</p>
+        <p>commit msg : {commit?.commit?.message}</p>
+        <p>commit date  : {commit?.commit?.committer.date}</p>
+        <p>commits</p>
+        </>
+    )
+}
+
+
+export default Commits;
