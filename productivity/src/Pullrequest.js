ad53425b091e447951d99beecc1b1a9ad30ecc50
Ayushi Jha <ayushi.jha@encora.com>
2024-02-17 18:48:47 +0530

    added pr and commit data

--- productivity/src/Pullrequest.js
+++ productivity/src/Pullrequest.js
@@ -1,22 +1,40 @@
 import React, { useEffect, useState } from "react";
+import "./Pullrequest.css";
 
 function Commits() {
   const [pr, setPr] = useState(null);
-  let length;
 
   useEffect(() => {
     fetch("https://api.github.com/repos/xic343-ayushijha/shop-cart/pulls")
       .then((res) => res.json())
       .then((data) => {
-        length=data.length;
-        return setPr(data[0]);
+        setPr(data);
       });
   }, []);
+  let list = pr?.map((elt) => {
+    return (
+      <li key={elt.id}>
+        <p>
+          <b>Assignee: </b>
+          {elt?.assignee?.login}
+        </p>
+        <p>
+          <b>Reviewer: </b> {elt.requested_reviewers[0]?.login}
+        </p>
+        <p>
+          <b>Updated at: </b> {elt.updated_at}
+        </p>
+        <p>
+          <b>url: </b> {elt.url}
+        </p>
+      </li>
+    );
+  });
   return (
     <>
-      <p>No. of PR: {length}</p>
-      <p>pull request date : {pr?.updated_at}</p>
-      <p>pr</p>
+          <p><b>No. of PR:</b> {pr?.length}</p>
+      <ol>{list}</ol>
+
     </>
   );
 }
